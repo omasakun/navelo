9da70b9701a417f8a0c1b80165db9a39bbf81978
o137 <git-user@o137.net>
2025-03-01 01:15:49 +0900

    feat: epaper display

--- esp32/src/main.rs
+++ esp32/src/main.rs
@@ -1,11 +1,328 @@
-fn main() {
+use std::{
+  thread::{sleep, spawn},
+  time::Duration,
+};
+
+use display::{Weact154Display, HEIGHT, WIDTH};
+use embedded_graphics::{
+  pixelcolor::BinaryColor,
+  prelude::*,
+  primitives::{Circle, PrimitiveStyle},
+};
+use esp_idf_hal::{
+  delay::Delay,
+  spi::{self, SpiDriver, SpiDriverConfig},
+};
+use esp_idf_hal::{gpio::AnyInputPin, prelude::*};
+use esp_idf_hal::{gpio::PinDriver, spi::SpiDeviceDriver};
+use esp_idf_svc::log::EspLogger;
+use esp_idf_svc::sys;
+use log::info;
+
+fn main() -> anyhow::Result<()> {
   // It is necessary to call this function once.
   // Otherwise some patches to the runtime implemented by esp-idf-sys might not link properly.
   // See https://github.com/esp-rs/esp-idf-template/issues/71
-  esp_idf_svc::sys::link_patches();
+  sys::link_patches();
+  EspLogger::initialize_default();
 
-  // Bind the log crate to the ESP Logging facilities
-  esp_idf_svc::log::EspLogger::initialize_default();
+  println!("Hello, world!");
+  spawn_heap_logger();
 
-  log::info!("Hello, world!");
+  let peripherals = Peripherals::take()?;
+  let delay = Delay::new_default();
+
+  let spi = peripherals.spi2;
+  let sclk = peripherals.pins.gpio19;
+  let sdo = peripherals.pins.gpio18;
+  let sdi = Option::<AnyInputPin>::None;
+  let cs = peripherals.pins.gpio16;
+
+  let reset = PinDriver::output(peripherals.pins.gpio20)?;
+  let busy = PinDriver::input(peripherals.pins.gpio21)?;
+  let dc = PinDriver::output(peripherals.pins.gpio17)?;
+
+  let spi_config = spi::config::Config::new().baudrate(20.MHz().into());
+  let spi_driver = SpiDriver::new(spi, sclk, sdo, sdi, &SpiDriverConfig::new())?;
+  let spi_device = SpiDeviceDriver::new(&spi_driver, Some(cs), &spi_config)?;
+
+  let mut display = Weact154Display::new(spi_device, dc, reset, busy, delay);
+
+  let mut x = 50;
+  let mut y = 50;
+  let mut dx = 6;
+  let mut dy = 8;
+  let radius: i32 = 20;
+
+  display.init()?;
+  display.clear(BinaryColor::On)?;
+  display.refresh_full()?;
+
+  loop {
+    display.clear(BinaryColor::On)?;
+
+    Circle::new(Point::new(x, y), radius as u32)
+      .into_styled(PrimitiveStyle::with_fill(BinaryColor::Off))
+      .draw(&mut display)?;
+
+    display.refresh_partial()?;
+
+    x += dx;
+    y += dy;
+
+    if x <= 0 || x + radius >= WIDTH as i32 {
+      dx = -dx;
+    }
+    if y <= 0 || y + radius >= HEIGHT as i32 {
+      dy = -dy;
+    }
+
+    sleep(Duration::from_millis(5000));
+  }
+}
+
+fn spawn_heap_logger() {
+  spawn(move || loop {
+    unsafe {
+      info!(
+        "free heap: {} (min: {})",
+        sys::esp_get_free_heap_size(),
+        sys::esp_get_minimum_free_heap_size()
+      );
+    }
+    sleep(Duration::from_millis(5000));
+  });
+}
+
+/**
+ * WeAct Studio Epaper Module 1.54 inch
+ * https://github.com/WeActStudio/WeActStudio.EpaperModule
+ * ZJY200200-0154DAAMFGN, SSD1681, 200x200, Black/White
+ */
+pub mod display {
+  use embedded_graphics::{
+    pixelcolor::BinaryColor,
+    prelude::{DrawTarget, OriginDimensions, Size},
+    Pixel,
+  };
+  use embedded_hal::{
+    delay::DelayNs,
+    digital::{InputPin, OutputPin},
+    spi::SpiDevice,
+  };
+  use thiserror::Error;
+
+  use self::command::*;
+
+  #[derive(Error, Debug)]
+  pub enum DisplayError {}
+
+  pub const WIDTH: u8 = 200;
+  pub const HEIGHT: u8 = 200;
+
+  pub struct Weact154Display<SPI, DC, RST, BSY, DLY> {
+    spi: SPI,
+    dc: DC,
+    reset: RST,
+    busy: BSY,
+    delay: DLY,
+    pixels: [u8; WIDTH as usize * HEIGHT as usize / 8],
+  }
+
+  impl<SPI, DC, RST, BSY, DLY> Weact154Display<SPI, DC, RST, BSY, DLY>
+  where
+    SPI: SpiDevice,
+    DC: OutputPin,
+    RST: OutputPin,
+    BSY: InputPin,
+    DLY: DelayNs,
+  {
+    pub fn new(spi: SPI, dc: DC, reset: RST, busy: BSY, delay: DLY) -> Self {
+      Self {
+        spi,
+        dc,
+        reset,
+        busy,
+        delay,
+        pixels: [0; WIDTH as usize * HEIGHT as usize / 8],
+      }
+    }
+
+    pub fn init(&mut self) -> Result<(), DisplayError> {
+      self.reset.set_low().unwrap();
+      self.delay(10);
+      self.reset.set_high().unwrap();
+      self.wait_busy()?;
+
+      self.send_command(SW_RESET)?;
+      self.wait_busy()?;
+
+      self.send_command(DRIVER_OUTPUT_CONTROL)?;
+      self.send_data(&[HEIGHT - 1, 0x00, 0x00])?;
+
+      self.send_command(DATA_ENTRY_MODE_SETTING)?;
+      self.send_data(&[0x03])?; // X/Y increment
+
+      // self.set_ram_area(0, 0, WIDTH, HEIGHT)?;
+
+      self.send_command(BORDER_WAVEFORM_CONTROL)?;
+      self.send_data(&[0x05])?; // white border (black: LSB=0)
+
+      self.send_command(TEMPERATURE_SENSOR_SELECTION)?;
+      self.send_data(&[0x80])?; // internal temperature sensor
+
+      self.wait_busy()?;
+      Ok(())
+    }
+    pub fn deep_sleep(&mut self) -> Result<(), DisplayError> {
+      self.wait_busy()?;
+      self.send_command(DISPLAY_UPDATE_CONTROL_2)?;
+      self.send_data(&[0x83])?; // disable analog/osc
+      self.send_command(MASTER_ACTIVATION)?;
+      self.send_command(NOP)?;
+
+      self.wait_busy()?;
+      self.send_command(DEEP_SLEEP_MODE)?;
+      self.send_data(&[0x01])?;
+      Ok(())
+    }
+    pub fn refresh_full(&mut self) -> Result<(), DisplayError> {
+      self.wait_busy()?;
+
+      self.set_ram_area(0, 0, WIDTH, HEIGHT)?;
+      self.send_command(WRITE_RAM)?;
+      self.send_pixels_data()?;
+
+      self.send_command(DISPLAY_UPDATE_CONTROL_1)?;
+      self.send_data(&[0x00])?; // display ram content
+      self.send_command(DISPLAY_UPDATE_CONTROL_2)?;
+      self.send_data(&[0xf7])?; // display with mode 1
+      self.send_command(MASTER_ACTIVATION)?;
+      self.send_command(NOP)?;
+      Ok(())
+    }
+    pub fn refresh_partial(&mut self) -> Result<(), DisplayError> {
+      self.wait_busy()?;
+
+      self.set_ram_area(0, 0, WIDTH, HEIGHT)?;
+      self.send_command(WRITE_RAM)?;
+      self.send_pixels_data()?;
+
+      self.send_command(DISPLAY_UPDATE_CONTROL_1)?;
+      self.send_data(&[0x00])?; // display ram content
+      self.send_command(DISPLAY_UPDATE_CONTROL_2)?;
+      self.send_data(&[0xff])?; // display with mode 2
+      self.send_command(MASTER_ACTIVATION)?;
+      self.send_command(NOP)?;
+      Ok(())
+    }
+
+    fn set_ram_area(&mut self, x: u8, y: u8, width: u8, height: u8) -> Result<(), DisplayError> {
+      self.send_command(SET_RAM_X_ADDRESS_START_END_POSITION)?;
+      self.send_data(&[x / 8, (x + width - 1) / 8])?;
+
+      self.send_command(SET_RAM_Y_ADDRESS_START_END_POSITION)?;
+      self.send_data(&[y, 0x00, y + height - 1, 0x00])?;
+
+      self.send_command(SET_RAM_X_ADDRESS_POSITION)?;
+      self.send_data(&[x / 8])?;
+
+      self.send_command(SET_RAM_Y_ADDRESS_POSITION)?;
+      self.send_data(&[y, 0x00])?;
+      Ok(())
+    }
+
+    fn delay(&mut self, ms: u32) {
+      DelayNs::delay_ms(&mut self.delay, ms);
+    }
+
+    fn send_command(&mut self, cmd: u8) -> Result<(), DisplayError> {
+      self.dc.set_low().unwrap();
+      self.delay(10);
+      self.spi.write(&[cmd]).unwrap();
+      Ok(())
+    }
+    fn send_data(&mut self, data: &[u8]) -> Result<(), DisplayError> {
+      self.dc.set_high().unwrap();
+      self.delay(10);
+      self.spi.write(data).unwrap();
+      Ok(())
+    }
+    fn send_pixels_data(&mut self) -> Result<(), DisplayError> {
+      self.dc.set_high().unwrap();
+      self.delay(10);
+      self.spi.write(&self.pixels).unwrap();
+      Ok(())
+    }
+    fn wait_busy(&mut self) -> Result<(), DisplayError> {
+      self.delay(10);
+      while self.busy.is_high().unwrap() {
+        // NOTE: make sure busy pin is correctly connected!
+        // println!("busy");
+        DelayNs::delay_ms(&mut self.delay, 10);
+      }
+      Ok(())
+    }
+
+    pub fn clear(&mut self, color: BinaryColor) -> Result<(), DisplayError> {
+      self.pixels.fill(u8::from(color.is_on()) * 0xFF);
+      Ok(())
+    }
+  }
+
+  impl<SPI, DC, RST, BSY, DLY> DrawTarget for Weact154Display<SPI, DC, RST, BSY, DLY>
+  where
+    SPI: SpiDevice,
+    DC: OutputPin,
+    RST: OutputPin,
+    BSY: InputPin,
+    DLY: DelayNs,
+  {
+    type Color = BinaryColor;
+    type Error = DisplayError;
+
+    fn draw_iter<I>(&mut self, pixels: I) -> Result<(), Self::Error>
+    where
+      I: IntoIterator<Item = Pixel<Self::Color>>,
+    {
+      for Pixel(coord, color) in pixels.into_iter() {
+        if coord.x < 0 || coord.x >= WIDTH as i32 || coord.y < 0 || coord.y >= HEIGHT as i32 {
+          continue;
+        }
+        let x = coord.x as usize;
+        let y = coord.y as usize;
+        let index = x + y * (WIDTH as usize);
+        let byte_index = index / 8;
+        let bit_index = index % 8;
+        let mask = 0b10000000 >> bit_index;
+        let color = u8::from(color.is_on()) << (7 - bit_index);
+        self.pixels[byte_index] = (self.pixels[byte_index] & !mask) | color;
+      }
+      Ok(())
+    }
+  }
+
+  impl<SPI, DC, RST, BSY, DLY> OriginDimensions for Weact154Display<SPI, DC, RST, BSY, DLY> {
+    fn size(&self) -> Size {
+      Size::new(WIDTH as u32, HEIGHT as u32)
+    }
+  }
+
+  mod command {
+    pub const DRIVER_OUTPUT_CONTROL: u8 = 0x01;
+    pub const DEEP_SLEEP_MODE: u8 = 0x10;
+    pub const DATA_ENTRY_MODE_SETTING: u8 = 0x11;
+    pub const SW_RESET: u8 = 0x12;
+    pub const TEMPERATURE_SENSOR_SELECTION: u8 = 0x18;
+    pub const MASTER_ACTIVATION: u8 = 0x20;
+    pub const DISPLAY_UPDATE_CONTROL_1: u8 = 0x21;
+    pub const DISPLAY_UPDATE_CONTROL_2: u8 = 0x22;
+    pub const WRITE_RAM: u8 = 0x24;
+    pub const BORDER_WAVEFORM_CONTROL: u8 = 0x3c;
+    pub const SET_RAM_X_ADDRESS_START_END_POSITION: u8 = 0x44;
+    pub const SET_RAM_Y_ADDRESS_START_END_POSITION: u8 = 0x45;
+    pub const SET_RAM_X_ADDRESS_POSITION: u8 = 0x4e;
+    pub const SET_RAM_Y_ADDRESS_POSITION: u8 = 0x4f;
+    pub const NOP: u8 = 0x7f;
+  }
 }
